e773a7f9c8a8dc4913580c98252412b01c71fe28
Jaja321 <benjajamin@gmail.com>
2020-05-28 21:26:44 +0300

    clean the code a little bit

--- netflix.js
+++ netflix.js
@@ -1,36 +1,36 @@
-const targetNode = document.body;
-
 const config = { childList: true, subtree: true };
+const seriesDurationKeywords = ['Season', 'Series', 'Part', 'Volume'];
+const seriesContainerClassKeywords = ['bob-card', 'jawBoneOpenContainer', 'jawBonePane'];
+
+const includesOneOf = (str, arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (str.includes(arr[i])) return true;
+  }
+  return false;
+};
 
 const callback = function (mutationsList) {
   for (let mutation of mutationsList) {
     for (let element of mutation.addedNodes) {
       const className = element.className;
-      if (
-        (className && className.includes('bob-card')) ||
-        className.includes('jawBoneOpenContainer') ||
-        className.includes('jawBonePane')
-      ) {
+      if (className && includesOneOf(className, seriesContainerClassKeywords)) {
         setTimeout(() => {
           const duration = element.querySelector('.duration')?.textContent;
-          if (!duration || !duration.includes('Season') && !duration.includes('Series') && !duration.includes('Parts')) {
+          if (!duration || !isSeriesDuration(duration, seriesDurationKeywords)) {
             //not a series.
             return;
           }
           const chartButton = getChartButton();
           const buttonContainer = element.querySelector('div[class~="video-meta"]');
           buttonContainer.appendChild(chartButton);
-          const title = element.querySelector('.bob-title')?.textContent || element.querySelector('img[class="logo"]')?.alt;
+          const title =
+            element.querySelector('.bob-title')?.textContent || element.querySelector('img[class="logo"]')?.alt;
           registerButton(title, chartButton, buttonContainer);
-  
         }, 300);
       }
     }
   }
 };
 
-// Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+observer.observe(document.body, config);
